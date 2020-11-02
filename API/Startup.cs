dbe9db628fcf738dc41ff644ec8e4988d49ecd41
Morgan Rees <morgan_rees@live.co.uk>
2020-11-02 14:27:03 +0000

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,8 @@ namespace API {
             });
 
             services.AddControllers();
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,8 @@ namespace API {
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
